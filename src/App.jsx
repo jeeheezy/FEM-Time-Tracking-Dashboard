b8a607d890edc2ad75a79a288c8cbc241d8b311a
Jeeho Lee <jeeho032399@gmail.com>
2023-10-29 17:07:47 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect} from 'react';
+
+// think about using useState to switch between day, week, month
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// // import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   let jsonData;
+//   fetch('data.json')
+//     .then(response => response.json())
+//     .then(data => {
+//       jsonData = data;
+//       processData(jsonData);
+//     })
+//     .catch(error => {
+//       console.error('Error:', error)
+//     });
+
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         {/* <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a> */}
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+export default App
